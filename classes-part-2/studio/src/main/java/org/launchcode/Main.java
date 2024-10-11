7220e15095381c09a307abe421db8f74fed712b3
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-10-10 20:33:54 -0500

    Main updated studio

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -3,6 +3,15 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+
+        MenuItem taco = new MenuItem("Taco", 3.75, "fish taco", "main course", true);
+
+//        Create menu items
+//        Create an ArrayList
+//        Add menuItems to ArrayList
+//        Create a new menu object
+//        Set the ArrayList as item of the menu
+
+       // Menu myMenu = new Menu();
     }
 }
