e6fc5037e8f56939e95b2384b366eb8c82001935
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-10-17 19:56:30 -0500

    Updated Brackets studio and finished tests

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,56 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void returnsTrueIfStringIsEmpty() {
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void returnsTrueIfStringHasBrackets(){
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void returnsTrueIfStringHasBracketsInBetween(){
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("Launch[code]"));
+    }
+    @Test
+    public void returnsTrueIfHasDoubleBrackets() {
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+    @Test
+    public void returnsTrueIfDoubleBracketsBeforeAndAfterString() {
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("[]LaunchCode[]"));
+    }
+    @Test
+    public void returnsTrueIfDoubleBracketsBeforeString() {
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void returnsTrueIfStringHasDoubleBrackets() {
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("[[Launchcode]]"));
+    }
+    @Test
+    public void returnsFalseIfStringHas1Bracket() {
+        assertEquals(false,BalancedBrackets.hasBalancedBrackets("[") );
+    }
+
+    @Test
+    public void returnsFalseForBackwardsBrackets() {
+        assertEquals(false,BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void returnsFalseIfStringHas1StartBracket() {
+        assertEquals(false,BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void returnsFalseIfStringHas2InternalBackwardsBracket() {
+        assertEquals(false,BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
 }
